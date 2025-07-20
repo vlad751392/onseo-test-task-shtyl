13c05bc567a73c214d3210032a447247612276ff
Vladyslav Shtyl <v.shtyl@twinwingames.com>
2025-07-20 13:38:42 +0300

    add config constant

---
+++ src/config/constant.ts
@@ -0,0 +1,17 @@
+export const NUM_FLOORS = 6;
+
+export const FLOOR_WIDTH = 300;
+export const FLOOR_HEIGHT = 80;
+export const FLOOR_SPACING = 0;
+export const FLOOR_CAPCITY = 7;
+export const BUILDING_PADDING = 20;
+
+export const ELEVATOR_WIDTH = 40;
+export const ELEVATOR_CAPACITY = 4;
+export const ELEVATOR_HEIGHT = 80;
+export const ELEVATOR_IDLE_TIME = 800;
+export const ELEVATOR_MOVE_TIME = 800;
+
+export const MIN_PERSON_SPAWN_TIME = 4000;
+export const MAX_PERSON_SPAWN_TIME = 10000;
+
